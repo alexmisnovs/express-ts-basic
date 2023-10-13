3f5cbde1ee6ce531b99d21e5b613c2d125a6702e
Alex macbookAir <aleksandrmisnov@gmail.com>
2023-10-13 10:28:19 +0100

    fixed bug with missing next() function in the corse

--- src/Server.ts
+++ src/Server.ts
@@ -10,6 +10,7 @@ export class Server {
     this.app.use(function (req: Request, res: Response, next: NextFunction) {
       res.header("Access-Control-Allow-Origin", "http://localhost:5173");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
     });
 
     this.app.set("port", process.env.PORT || 8000);
